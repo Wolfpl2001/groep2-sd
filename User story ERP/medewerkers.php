813bd38b6b8418da393dc484752e763841520b84
kokloler <jelte.kessels@gmail.com>
2023-06-06 14:56:22 +0200

    updated melvins code

--- User story ERP/medewerkers.php
+++ User story ERP/medewerkers.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <?php
   //Connect to Database
-  $connect = new mysqli("localhost:3306", "root", "", "medewerkers_database") or die("Database not connected");
+  include "config.php";
   $sql = "SELECT * FROM werkzaamheden;";
-  $result = mysqli_query($connect, $sql);
+  $result = mysqli_query($conn, $sql);
   $resultCheck = mysqli_num_rows($result);
 ?>
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="medewerkers.css">
         <!--Connect to Bootstrap-->
         <link href=https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <script src=https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
