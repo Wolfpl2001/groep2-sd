e347c586d5605cde8155ebf859bce3d0b03966ce
kokloler <jelte.kessels@gmail.com>
2023-06-14 11:42:46 +0200

    fixed bug with timer to speed up the time
    also linking things

--- User story ERP/URS/URS.php
+++ User story ERP/URS/URS.php
@@ -51,9 +51,9 @@ if (isset($_POST['Login'])) {
         <div class="form-box">
             <div class="form-value">
                 <form action="index.php" method="post">
-                    <a href= "main.php"><ion-icon name="arrow-back-outline" class="arrow"></ion-icon></a>
-                    <a href= "URS.php?lang=nl"><img src="img/NL.png" alt="NL Flag" class="flag-nl" href="nl-index.html"></a>
-                    <a href= "URS.php?lang=en"> <img src="img/eng.png" alt="ENG Flag" class="flag-en used"></a>
+                    <a href= "../mains/main.php"><ion-icon name="arrow-back-outline" class="arrow"></ion-icon></a>
+                    <a href= "URS.php?lang=nl"><img src="../img/NL.png" alt="NL Flag" class="flag-nl" href="nl-index.html"></a>
+                    <a href= "URS.php?lang=en"> <img src="../img/eng.png" alt="ENG Flag" class="flag-en used"></a>
                     <h2><?php echo $lang['reg_hours']?></h2>
                     <?php
                     if (isset($error)) {
@@ -70,7 +70,7 @@ if (isset($_POST['Login'])) {
                 <button id="start"> start timer </button>
                 <br>
                 <br>
-                <button id="stop"> stop timer </button> <br> <br>
+                <button id="stopbtn"> stop timer </button> <br> <br>
                 <button id="reset">reset timer </button>
 
             </div>
--- User story ERP/URS/scriptsURS.js
+++ User story ERP/URS/scriptsURS.js
@@ -18,24 +18,33 @@ function check() {
     else {
     clearInterval(myInterval)
 }};
+
+function stopfunction() {
+    clearInterval(myInterval)
+    console.log("test 2")
+    document.getElementById("start").style.display = "unset";
+    document.getElementById("stopbtn").style.display = "none";
+};
 // start button
 // bug with timer cuz you can spam click the button
 const startBtn = document.querySelector("#start");
 startBtn.addEventListener("click", function (event) {
     myInterval = setInterval(time, 1000);
     console.log("start button press")
+    document.getElementById("start").style.display = "none";
+    document.getElementById("stopbtn").style.display = "unset";
 });
 
 //stop button
-const stopBtn = document.querySelector("#stop");
+const stopBtn = document.querySelector("#stopbtn");
 stopBtn.addEventListener("click", function (event) {
-    clearInterval(myInterval)
-    console.log("test 2")
+    stopfunction()
 });
 const resetBtn = document.querySelector("#reset")
 resetBtn.addEventListener("click", function (event) {
     timer = 0
     console.log(timer)
     document.getElementById("demo").innerHTML = timer;
+    stopfunction()
 });
 
